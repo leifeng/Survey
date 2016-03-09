52e8d721218fb7a8b497ebe692376bb717b2882b
leifeng <289528400@qq.com>
2016-03-09 17:43:06 +0800

    目录搭建

---
+++ f2e/webpack.config.dev.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            }]
+    }
+};
